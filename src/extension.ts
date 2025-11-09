4e1509b1165b4e30e8823924ad0c56ea30f453ad
Sebastian <s.shing.smith@gmail.com>
2025-11-09 01:58:41 -0500

    idk

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,6 @@ import {Compile} from "./python";
 let vidDict = {"oversensory":"7568990861531385143", "peachyslime":"7570402732197137677","nocopyrightgameplay2":"7467001786117672214","familyguy.v01":"7570028390435179779"}
 
 export async function activate(context: vscode.ExtensionContext) {
-  	let panel: vscode.WebviewPanel | undefined = undefined;	
 	console.log('Congratulations, your extension "brainrot-extension" is now active!');
 	// vscode.commands.registerCommand('brainrot.tiktok', () => {
 		
@@ -13,15 +12,13 @@ export async function activate(context: vscode.ExtensionContext) {
 		const editor = vscode.window.activeTextEditor;
 		Compile(editor?.document.fileName as string);
 	})
-	if (panel){
-		panel.reveal(vscode.window.activeTextEditor);
-	} else{
-	 panel = vscode.window.createWebviewPanel('tiktok', 'Tiktok Brainrot', vscode.ViewColumn.Beside,{enableScripts: true}) 
+
+	let panel = vscode.window.createWebviewPanel('tiktok', 'Tiktok Brainrot', vscode.ViewColumn.Beside,{enableScripts: true}) 
 for (const [key, value] of Object.entries(vidDict)) {	
 	 panel.webview.html = getWebviewContent(key,value);
 	await delay(15000);
 
-}};
+};
 
 
 }
@@ -31,12 +28,14 @@ function delay(ms: number) {
 function getWebviewContent(username:string, vidId:string){
 	return `<blockquote 
 				class="tiktok-embed" 
+				cite="https://www.tiktok.com/@${username}/video/${vidId}"
 				data-video-id="${vidId}" 
 				style="max-width: 320px; max-height:600px;" 
 			>
 				<section></section>
 			</blockquote>
 			<script async src="https://www.tiktok.com/embed.js"></script>`
+			
 }
 
 // This method is called when your extension is deactivated
