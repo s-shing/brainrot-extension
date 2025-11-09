9b74c09d39a65156db34a09bc19cb06be6cfa672
Ian <westpfal6776@gmail.com>
2025-11-08 22:32:10 -0500

    Restructuring 'compiler'

--- brainrot.h
+++ brainrot.h
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-# define fuck int main(int argc, char **argv) {
-# define it
-# define we
-# define ball
-
-# define set(x, y, z) x y = z
-
-# define yap printf
-
-# define serving for(
-# define until ;
-# define by ;
-# define yk ){
-
-# define sus if(
-# define or } else if (
-# define maybe 
-# define otherwise } else {
-
+// Constants
 # define no !
 # define cap false
+# define DEAD 
+# define aura *
+# define glaze &
 
-# define DEAD }
-# define SOB 
-# define BROKEN
-# define WILT
 
-# define crash return
-# define out
+// Basic operations
+# define lore(type, var, val) type var = val
+# define yap(x) printf(x)
+# define yap_int(var) printf("%d\n", var)
+# define six_seven(var, inc) var += inc
+# define sigma(var, val) var == val
+# define alpha(var, val) var > val
+# define beta(var, val) var < val
+# define crash_out(x) return x
+# define tweak return
 
-# define alpha >
-# define beta <
-# define is ==
+// Functions and Environments
+# define fuckItWeBall(body) int main(int argc, char** argv) { body }
+# define sus(cond, body, otherwise) if( cond ) { body } else { otherwise }
+# define serving(init, cond, inc, body) for( init ; cond ; inc ) { body }
+# define stan(cond, body) serving(DEAD; cond; DEAD) { body }
+# define rizz(type, name, args, body) type name args { body }
 
-# define bump(x) x++
+
+// # define SOB
+// # define BROKEN
+// # define WILT
+
+
